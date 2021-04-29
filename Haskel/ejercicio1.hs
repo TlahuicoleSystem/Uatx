509380da347030bf1a00f80db505e11ddc3c2e47
TlahuicoleSystem <barrales.josue.1i@gmail.com>
2021-04-28 20:12:43 -0500

    ac

--- Haskel/ejercicio1.hs
+++ Haskel/ejercicio1.hs
@@ -5,3 +5,11 @@ condicion x = if x > 100 then x else x*2
 
 sumaTres :: Float -> Float -> Float -> Float
 sumaTres x y z = x + y + z
+
+mayor :: Float -> Float -> Float
+mayor x y = if x > y then x else y
+
+mayor3 :: Float -> Float -> Float -> Float
+mayor3 x y z = mayor x (mayor y z)
+mayor4 :: Float -> Float -> Float -> Float -> Float
+mayor4 w x y z = mayor (mayor w x) (mayor y z)
