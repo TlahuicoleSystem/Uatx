32eb8bb9ae3c846e0257f3e72a9821caf2a83b19
TlahuicoleSystem <barrales.josue.1i@gmail.com>
2021-04-28 20:09:10 -0500

    actualizacion

--- Haskel/ejercicio1.hs
+++ Haskel/ejercicio1.hs
@@ -3,5 +3,5 @@ sumaDos x = x + 2
 msDos x y = sumaDos x + sumaDos y
 condicion x = if x > 100 then x else x*2
 
-sumaTres :: Int -> Int -> Int -> Int
+sumaTres :: Float -> Float -> Float -> Float
 sumaTres x y z = x + y + z
