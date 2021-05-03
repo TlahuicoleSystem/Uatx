d0d6664955efde2324d82f2d80ab9d2f2cdc550e
TlahuicoleSystem <barrales.josue.1i@gmail.com>
2021-05-03 15:01:10 -0500

    ya wey

---
+++ Haskel/dibujoslis.hs
@@ -0,0 +1,6 @@
+import Graphics.Gloss
+main :: IO ()
+main = display (InWindow "Circulo concentrico" (300,300) (20,20)) white
+circulo
+circulo :: Picture
+circulo = pictures [ circle x | x <- [10,20..100] ]
--- Haskel/ejercicio4G.hs
+++ Haskel/ejercicio4G.hs
@@ -2,4 +2,4 @@ import Graphics.Gloss
 main :: IO ()
 main = display (InWindow "Funciones Predefinidas" (300,300) (20,20)) white circulo
 circulo :: Picture
-circulo = thickCircle 50 10
+circulo = Pictures [thickCircle 50 10, circle 100, Polygon [(72,72),(144,72),(144,144),(72,144),(72,72)]]
