3d59df5586a7c387d55d5f66588e7f6f58f1e33e
TlahuicoleSystem <barrales.josue.1i@gmail.com>
2021-05-03 15:08:57 -0500

    ya we

--- Haskel/dibujoslis.hs
+++ Haskel/dibujoslis.hs
@@ -1,6 +1,5 @@
 import Graphics.Gloss
 main :: IO ()
-main = display (InWindow "Circulo concentrico" (300,300) (20,20)) white
-circulo
+main = display (InWindow "Circulo concentrico" (300,300) (20,20)) white circulo
 circulo :: Picture
 circulo = pictures [ circle x | x <- [10,20..100] ]
